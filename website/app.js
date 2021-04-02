f21e4afe54284e5e0ed975a2f7566ea5f58e5585
Borislav Yordanov <brobos@gmail.com>
2021-04-02 18:40:34 +0300

    Create app.js

---
+++ website/app.js
@@ -0,0 +1,68 @@
+/* Global Variables */
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '72bacd0a5750931385c2440c0e23ad65';
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// create click event to button
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+
+  let newZip = document.getElementById('zip').value;
+  let userText = document.getElementById('feelings').value;
+
+  getWeather(baseURL, newZip, apiKey)
+    .then(function (data) {
+      // add data to POST request
+      postData('/add', { date: newDate, temp: data.main.temp, userText });
+      updateUI('/all');
+    })
+};
+
+// function GET data
+const getWeather = async (baseURL, newZip, apiKey) => {
+  // res equals to the result of fetch function
+  const response = await fetch(baseURL + newZip + apiKey);
+  try {
+    // data equals to the result of fetch function
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// function POST data
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  };
+};
+
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try {
+    const allData = await request.json();
+    document.getElementById('date').innerHTML = allData[0].date;
+    document.getElementById('temp').innerHTML = allData[0].temp;
+    document.getElementById('content').innerHTML = allData[0].userText;
+  }
+  catch (error) {
+    console.log("error", error);
+  }
+};
